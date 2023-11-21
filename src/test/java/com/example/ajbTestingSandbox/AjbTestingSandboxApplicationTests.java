7f4df103cb8da6b918642b99cba47b240295f029
Andrew Branch <branch.andrew2517@att.net>
2023-11-20 21:09:48 -0600

    STABLE Build #1

--- src/test/java/com/example/ajbTestingSandbox/AjbTestingSandboxApplicationTests.java
+++ src/test/java/com/example/ajbTestingSandbox/AjbTestingSandboxApplicationTests.java
@@ -1,5 +1,9 @@
 package com.example.ajbTestingSandbox;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +14,30 @@ class AjbTestingSandboxApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	public void testCheckInteger(){
+		assertEquals(42, Integer.sum(19, 23));
+	}
+	@Test
+	public void testDivide() {
+    	assertThrows(ArithmeticException.class, () -> {
+        Integer.divideUnsigned(42, 0);
+    });
+};
+
+public class CalculatorJUnit5Test {
+    @Tag("slow")
+    @Test
+    public void testAddMaxInteger() {
+        assertEquals(2147483646, Integer.sum(2147183646, 300000));
+    }
+ 
+    @Tag("fast")
+    @Test
+    public void testDivide() {
+        assertThrows(ArithmeticException.class, () -> {
+            Integer.divideUnsigned(42, 0);
+        });
+    }
 }
+};
