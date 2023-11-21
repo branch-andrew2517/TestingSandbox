8a3f898b9a73b578afa07782bd5d749824e76b5b
branch-andrew2517 <branch.andrew2517@att.net>
2023-11-20 18:15:08 -0600

    STABLE version applied

---
+++ src/main/java/com/example/ajbTestingSandbox/AjbTestingSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.ajbTestingSandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjbTestingSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjbTestingSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ajbTestingSandbox/AjbTestingSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ajbTestingSandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjbTestingSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
